be27f2f921f46717da6f80fe96ac7f0559ce588f
Sicknemesis <hulluhalonen10@gmail.com>
2014-09-28 20:41:07 +0300

    Toolchain and defconfig editing
    Change-Id: I49b44893193fb6b8d7b6e3b496c990fcad014c80

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,8 @@
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+TARGET_KERNEL_CONFIG := ak_bacon_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 
 # Bluetooth
